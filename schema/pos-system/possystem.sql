00857b30a15f6ab0c1458c0224338525e2417849
r3inaQuest <reina@ye-t.net>
2025-04-03 14:27:24 +0700

    データベース編集

--- schema/pos-system/possystem.sql
+++ schema/pos-system/possystem.sql
@@ -3,7 +3,7 @@ SET SESSION FOREIGN_KEY_CHECKS=0;
 
 /* Create Tables */
 
-CREATE TABLE admin_users
+CREATE TABLE admin_login
 (
 	-- 管理者ID
 	id int NOT NULL AUTO_INCREMENT COMMENT '管理者ID',
@@ -17,38 +17,7 @@ CREATE TABLE admin_users
 );
 
 
-CREATE TABLE cart
-(
-	-- カートID
-	id int NOT NULL AUTO_INCREMENT COMMENT 'カートID',
-	-- 購入数
-	quantity int NOT NULL COMMENT '購入数',
-	-- 追加日時
-	created_at timestamp NOT NULL COMMENT '追加日時',
-	-- ユーザーID
-	user_id int NOT NULL COMMENT 'ユーザーID',
-	-- 商品ID
-	product_id int NOT NULL COMMENT '商品ID',
-	PRIMARY KEY (id)
-);
-
-
 CREATE TABLE orders
-(
-	-- 注文ID
-	id int NOT NULL AUTO_INCREMENT COMMENT '注文ID',
-	-- 税込み合計金額
-	total_price decimal(10,2) NOT NULL COMMENT '税込み合計金額',
-	tax_amount decimal(10,2) NOT NULL,
-	-- 注文日時
-	created_at timestamp NOT NULL COMMENT '注文日時',
-	-- ユーザーID
-	user_id int NOT NULL COMMENT 'ユーザーID',
-	PRIMARY KEY (id)
-);
-
-
-CREATE TABLE order_items
 (
 	-- 注文明細ID
 	id int NOT NULL AUTO_INCREMENT COMMENT '注文明細ID',
@@ -56,16 +25,12 @@ CREATE TABLE order_items
 	quantity int NOT NULL COMMENT '購入数',
 	-- 税抜き価格
 	price decimal(10,2) NOT NULL COMMENT '税抜き価格',
-	-- 消費税率
-	tax_rate decimal(5,2) NOT NULL COMMENT '消費税率',
-	-- 消費税額
-	tax_amount decimal(10,2) NOT NULL COMMENT '消費税額',
 	-- 税込み価格
 	total_price decimal(10,2) NOT NULL COMMENT '税込み価格',
 	-- 商品ID
 	product_id int NOT NULL COMMENT '商品ID',
-	-- 注文ID
-	order_id int NOT NULL COMMENT '注文ID',
+	-- ユーザーID
+	user_id int NOT NULL COMMENT 'ユーザーID',
 	PRIMARY KEY (id)
 );
 
@@ -78,8 +43,6 @@ CREATE TABLE products
 	name varchar(255) NOT NULL COMMENT '商品名',
 	-- 税抜き価格
 	price decimal(10,2) NOT NULL COMMENT '税抜き価格',
-	-- 消費税率（例: 0.10 = 10%）
-	tax_rate decimal(5,2) NOT NULL COMMENT '消費税率（例: 0.10 = 10%）',
 	-- 在庫数
 	stock int NOT NULL COMMENT '在庫数',
 	-- 登録日時
@@ -111,33 +74,9 @@ CREATE TABLE users
 
 /* Create Foreign Keys */
 
-ALTER TABLE order_items
-	ADD FOREIGN KEY (order_id)
-	REFERENCES orders (id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE cart
+ALTER TABLE orders
 	ADD FOREIGN KEY (product_id)
 	REFERENCES products (id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE order_items
-	ADD FOREIGN KEY (product_id)
-	REFERENCES products (id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE cart
-	ADD FOREIGN KEY (user_id)
-	REFERENCES users (id)
 	ON UPDATE RESTRICT
 	ON DELETE RESTRICT
 ;
--- web/pos-system/db.php
+++ web/pos-system/db.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $pdo = new PDO('mysql:host=mysql; dbname=pos-system; charset=utf8', 'root', '');
+    $pdo = new PDO('mysql:host=mysql; dbname=possystem; charset=utf8', 'root', '');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'DB接続エラー: ' . $e->getMessage();
